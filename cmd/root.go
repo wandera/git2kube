e505bce79a948ae469863ceac53a25799caae427
Jakub Coufal <jakub.coufal@ibacz.eu>
2018-05-01 16:02:08 +0200

    Logging improvement

--- cmd/root.go
+++ cmd/root.go
@@ -2,16 +2,31 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	log "github.com/sirupsen/logrus"
+	"fmt"
 )
+
+var loglevel string
 
 var rootCmd = &cobra.Command{
 	Use:               "git2kube",
 	DisableAutoGenTag: true,
 	Short:             "Git to ConfigMap conversion tool",
 	Long:              `Commandline tool for loading files from git repository into K8s ConfigMap`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		lvl, err := log.ParseLevel(loglevel)
+		if err != nil {
+			return err
+		}
+
+		log.SetLevel(lvl)
+		return nil
+	},
 }
 
 func init() {
+	rootCmd.PersistentFlags().StringVarP(&loglevel, "log-level", "l", "info", fmt.Sprintf("command log level (options: %s)", log.AllLevels))
+
 	rootCmd.AddCommand(loadCmd)
 	rootCmd.AddCommand(watchCmd)
 	rootCmd.AddCommand(genDocCmd)
--- main.go
+++ main.go
@@ -17,5 +17,4 @@ func main() {
 func init() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetOutput(os.Stdout)
-	log.SetLevel(log.InfoLevel)
 }
